2a96424afa4783fd2594f917984ef7c6dd949f10
Redwann <redwannmelsa@gmail.com>
2024-04-17 10:35:15 +0200

    setting up project structure

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+require("./app/models/index.js");
+
+const app = express();
+
+module.exports = app;
--- app/models/index.js
+++ app/models/index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv').config()
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { ssl: false })
   .then(() => console.log('MongoDB connected !'))
